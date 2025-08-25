1c14162a1026e0ca72657f8f152af65fa44c415c
prakash-BackendDeveloper <prakash96m@gmail.com>
2025-08-25 17:36:44 +0530

    Multiple users added

--- SpringSecurityService/src/main/java/com/example/SpringSecurityService/configuration/SecurityConfiguration.java
+++ SpringSecurityService/src/main/java/com/example/SpringSecurityService/configuration/SecurityConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,5 +23,29 @@ public class SecurityConfiguration {
         http.httpBasic(Customizer.withDefaults());
         http.sessionManagement((session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1=User.withDefaultPasswordEncoder()
+                .username("rohit")
+                .password("3000")
+                .roles("USER")
+                .build();
+
+        UserDetails user2=User.withDefaultPasswordEncoder()
+                .username("kohli")
+                .password("2000")
+                .roles("USER")
+                .build();
+
+        UserDetails user3=User.withDefaultPasswordEncoder()
+                .username("pant")
+                .password("1000")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,user2,user3);
     }
 }
