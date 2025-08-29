43f63b1b7fc0d61ff0b90efb25114d5ec296d5cf
prakash-BackendDeveloper <prakash96m@gmail.com>
2025-08-29 18:48:39 +0530

    jwt authenticated

--- SpringSecurityService/src/main/java/com/example/SpringSecurityService/configuration/SecurityConfiguration.java
+++ SpringSecurityService/src/main/java/com/example/SpringSecurityService/configuration/SecurityConfiguration.java
@@ -18,6 +18,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,19 +30,23 @@ public class SecurityConfiguration {
     @Autowired
     UserDetailsService userDetailsService;
 
+    @Autowired
+    private  JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(customer -> customer.disable());
         http.authorizeHttpRequests(request -> request
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .requestMatchers("/user/**").hasAnyRole("ADMIN","USER")
-                .requestMatchers("/public/**").permitAll()
-                .requestMatchers("login", "register").permitAll()
+//                .requestMatchers("/admin/**").hasRole("ADMIN")
+//                .requestMatchers("/user/**").hasAnyRole("ADMIN","USER")
+//                .requestMatchers("/public/**").permitAll()
+                .requestMatchers("/login", "/register").permitAll()
                 .anyRequest().authenticated());
 
 //        http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         http.sessionManagement((session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
--- SpringSecurityService/src/main/java/com/example/SpringSecurityService/controller/StudentController.java
+++ SpringSecurityService/src/main/java/com/example/SpringSecurityService/controller/StudentController.java
@@ -25,7 +25,7 @@ public class StudentController {
         return students;
     }
 
-    @PostMapping("/students")
+    @PostMapping("/admin/students")
     public void addStudent(@RequestBody Student student){
         students.add(student);
     }
